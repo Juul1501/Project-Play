2cf889a99d0b241353cfad48ec5c50157fbc11cd
Juul Cox <juulcox@manus-vr.com>
2020-10-26 17:12:30 +0100

    initial commit

---
+++ Assets/Chicken.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+public class Chicken : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public LayerMask layer;
+
+    bool exploded = false;
+    Vector3 destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform t = GetClosestBuilding(transform.position,10);
+        destination = t.position;
+        agent.SetDestination(destination);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Explode(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius, layer);
+        foreach (var hitCollider in hitColliders)
+        {
+            hitCollider.gameObject.GetComponent<Farm>().AddDamage(500);
+        }
+    }
+    Transform GetClosestBuilding(Vector3 center, float radius)
+    {
+        Transform tMin = null;
+        float minDist = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius, layer);
+        foreach (var h in hitColliders)
+        {
+            float dist = Vector3.Distance(h.transform.position, currentPos);
+            if (dist < minDist)
+            {
+                tMin = h.transform;
+                minDist = dist;
+            }
+        }
+
+        return tMin;
+    }
+}
---
+++ Assets/Scripts/AnimalSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSpawner : MonoBehaviour
+{
+    public int amount = 20;
+    public GameObject chicken;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                if (hit.transform != null)
+                {
+                    if (amount > 0)
+                    {
+                        Debug.Log("Hit " + hit.transform.gameObject.name);
+                        Instantiate(chicken, hit.point, Quaternion.identity, hit.transform);
+                        amount -= 1;
+                    }
+                }
+        }
+    }
+}
