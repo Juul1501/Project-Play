4cb8daa18d5d8d0d795177607edb214c7fbc09b9
Juul Cox <juulcox00@gmail.com>
2020-11-03 17:07:47 +0100

    level manager update

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -7,7 +7,6 @@ public class LevelManager : MonoBehaviour
 {
     public string level;
     public Bounds bound;
-
     void Start()
     {
 
@@ -21,7 +20,7 @@ public class LevelManager : MonoBehaviour
             Debug.Log("level Complete");
             StartCoroutine(LoadScene(level));
         }
-        bound = new Bounds(transform.position, Vector3.zero);
+        bound = new Bounds();
         for (int i = 0; i < transform.childCount; i++)
         {
             Transform t  = transform.GetChild(i);
