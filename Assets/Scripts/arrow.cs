9293299291bd19a92810708dfa0b48bb4151ee41
Juul Cox <36227603+Juul1501@users.noreply.github.com>
2020-11-04 17:19:48 +0100

    Revert "Revert "added tutorial""
    This reverts commit 2a043bc16452da2a0066b659eaf70b44537d7e54.

---
+++ Assets/Scripts/arrow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow : MonoBehaviour
+{
+    public float amplitudeY = 0.9f;
+    public float omegaY = 2f;
+    public float offset = 0.2f;
+    
+    float index;
+    public void Update()
+    {
+        index += Time.deltaTime;
+        float y = Mathf.Abs(amplitudeY * Mathf.Sin(omegaY * index));
+        transform.localPosition = new Vector3(transform.position.x, y+offset, transform.position.z);
+    }
+}
